2e06ce73cacc5798aed1eba8f1dca686a3d415b4
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-26 22:48:51 +0530

    link

--- src/pages/Skincare.jsx
+++ src/pages/Skincare.jsx
@@ -12,19 +12,21 @@ function Skincare() {
             <h5>Personalized Facials</h5>
             <p className="px-4 mt-4"><b>No two faces are alike.</b>We get to know you and your skin to tailor the right treatment to help you reach your skincare goals.</p>
             <br/>
-            <p>shop now</p>
+            <p><a href="https://app.mulearn.org/profile/arunknair@mulearn" style={{ textDecoration: 'none' }}>shop now</a></p>
+
         </div>
         <div className="col">
             <h5>Curated Products</h5>
             <p className="px-4 mt-4"><b>What you use matters.</b>Ingredients are at the core of skincare, so we educate you and recommend you the best products.</p>
             <br/>
-            <p>shop now</p>
+            <p><a href="https://app.mulearn.org/profile/arunknair@mulearn" style={{ textDecoration: 'none' }}>shop now</a></p>
+
         </div>
         <div className="col">
             <h5>Personalized Facials</h5>
             <p className="px-4 mt-4"><b>Skincare is our obsession.</b>We're a team of Licensed Esteticians who can't wait to share what we know with you.</p>
             <br/>
-            <p >shop now</p>
+            <p><a href="https://app.mulearn.org/profile/arunknair@mulearn" style={{ textDecoration: 'none' }}>shop now</a></p>
         </div>
        
       </div>
