3b9724081d2447bccb236129f31b4c2ec1852a80
YoungGuy2003 <abhijithts402@gmail.com>
2023-10-27 00:30:55 +0530

    done

--- src/pages/Spa.jsx
+++ src/pages/Spa.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import './Spa.css';
 
 function Spa() {
-
     return (
-        <div className="container">
+        <div className="card-container">
             <div className="row">
-                <div className="col">
-                    <h1>Enquire</h1>
+                <div className="col-4">
+                    <div className="card custom-card">
+                        <div className="card-body">
+                            <h5 className="card-title1"><b>We Offer A Wide</b></h5>
+                            <h5 className="card-title1"><b>Selection Of</b></h5>
+                            <h5 className="card-title1"><b>Spa Services</b></h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="card custom-card card-right">
+                        <div className="card-body1">
+                            <p className="card-text"><b>Body treatments care for the skin of the whole body! Whether you're looking to slough your skin to perfection, or tone & detoxify your whole body, we've got a treatment for you. Lorem Ipsum is simply dummy text of the printing....</b></p>
+                            <button className="btn btn-primary float-start">Learn More</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="card custom-card card-right">
+                        <div className="card-body1">
+                            <p className="card-text">Body treatments care for the skin of the whole body! Whether you're looking to slough your skin to perfection, or tone & detoxify your whole body, we've got a treatment for you. Lorem Ipsum is simply dummy text of the printing....</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/reflexology_massage.jsx
+++ src/pages/reflexology_massage.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import "./reflex.css";
 
 function reflexology_massage() {
-
     return (
-        <div className="container">
+        <div className="card-container11">
             <div className="row">
-                <div className="col">
-                    <h1>Enquire</h1>
+                <div className="col-4">
+                    <div className="card custom-card11">
+                        <img
+                            src="https://facesspa.com/wp-content/uploads/2020/07/AdobeStock_143330491.jpeg"
+                            alt="Card 1"
+                            className="custom-image"
+                        />
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="card custom-card11">
+                        <img
+                            src="https://facesspa.com/wp-content/uploads/2020/07/AdobeStock_143330491.jpeg"
+                            alt="Card 2"
+                            className="custom-image"
+                        />
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="card custom-card">
+                        <div className="card-body">
+                            <h5 className="card-title1"><b>REFELXOLOGY</b></h5>
+                            <h5 className="card-title1"><b>MASSAGE</b></h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
