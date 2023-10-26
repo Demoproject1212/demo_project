a0d922994a7e9ee2204baf1c74ca242f36eb0da2
YoungGuy2003 <abhijithts402@gmail.com>
2023-10-26 23:29:31 +0530

    hero done

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import './hero.css';
 
-function hero() {
-
+function Hero() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h1>Enquire</h1>
+        <div className="row">
+            <div className="col">
+                <div className="backg">
+                    <div className="content">
+                        <div className="content-container">
+                            <h1>SPA &</h1>
+                            <h1>MASSAGE</h1>
+                            <div className="para">
+                                <p>take a little time for yourself.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
 
-export default hero;
+export default Hero;
