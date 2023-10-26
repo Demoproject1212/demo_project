71d9354a2d44f704207826c81bc4d3008cab891f
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-26 23:20:51 +0530

    natural

--- src/pages/natural.jsx
+++ src/pages/natural.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import Ni from "../images/natural.png";
 
-function natural() {
-
+function Natural() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h1>Enquire</h1>
-                </div>
-            </div>
-        </div>
+        <Container fluid className="mt-5">
+            <Row>
+                {/* Left Column */}
+                <Col md={4} className="d-flex flex-column justify-content-center align-items-start pl-5">
+                <h1 className="mb-1 border">
+    THE POWER OF NATURE
+</h1>
+                    <p className="text-left mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim, non convallis felis mattis at.</p>
+                    <Button variant="dark">Learn more</Button>
+                </Col>
+
+                {/* Right Column */}
+                <Col md={8} className="d-flex flex-wrap justify-content-end pr-5">
+                    <img src={Ni} alt="Natural" className="img-fluid" />
+
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
-export default natural;
+export default Natural;
