c458241f7e8851bf0e30d8ba6d1c6f80912a955f
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-10-26 22:43:18 +0530

    Update Skincare.jsx

--- src/pages/Skincare.jsx
+++ src/pages/Skincare.jsx
@@ -3,26 +3,28 @@ import React from "react";
 function Skincare() {
   return (
     <div className="container">
+    <div className="row">
       <h1>WE PERSONALIZE SKINCARE</h1>
       <p>to make a postive impact in people's lives</p>
-      <div className="row">
-        <div className="col mx-auto p-2">
+    </div>
+      <div className="row px-5 mt-5">
+        <div className="col">
             <h5>Personalized Facials</h5>
-            <p><b>No two faces are alike.</b>We get to know you and your skin to tailor the right treatment to help you reach your skincare goals</p>
+            <p className="px-4 mt-4"><b>No two faces are alike.</b>We get to know you and your skin to tailor the right treatment to help you reach your skincare goals.</p>
             <br/>
-            <p><a>shop now</a></p>
+            <p>shop now</p>
+        </div>
+        <div className="col">
+            <h5>Curated Products</h5>
+            <p className="px-4 mt-4"><b>What you use matters.</b>Ingredients are at the core of skincare, so we educate you and recommend you the best products.</p>
+            <br/>
+            <p>shop now</p>
         </div>
         <div className="col">
             <h5>Personalized Facials</h5>
-            <p><b>No two faces are alike.</b>We get to know you and your skin to tailor the right treatment to help you reach your skincare goals</p>
+            <p className="px-4 mt-4"><b>Skincare is our obsession.</b>We're a team of Licensed Esteticians who can't wait to share what we know with you.</p>
             <br/>
-            <p><a>shop now</a></p>
-        </div>
-        <div className="col">
-            <h5>Personalized Facials</h5>
-            <p><b>No two faces are alike.</b>We get to know you and your skin to tailor the right treatment to help you reach your skincare goals</p>
-            <br/>
-            <p><a>shop now</a></p>
+            <p >shop now</p>
         </div>
        
       </div>
