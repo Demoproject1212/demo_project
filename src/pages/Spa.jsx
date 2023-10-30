71ecb6e2837190d5678e28ac445f4bffd07f0d24
YoungGuy2003 <abhijithts402@gmail.com>
2023-10-30 21:09:30 +0530

    made changes according to ajmal bro in my section

--- src/pages/Spa.jsx
+++ src/pages/Spa.jsx
@@ -1,35 +1,50 @@
 import React from "react";
 import './Spa.css';
+
 const cardData = [
     {
         title: "WE OFFER A WIDE",
-        subtitle: "SELECTION OF",
-        description: "SPA SERVICES"
+        title2: "SELECTION OF",
+        title3: "SPA SERVICES"
     },
     {
-        title: "Body treatments care for the skin of the whole body!",
-        subtitle: "Whether you're looking to slough your skin to perfection, or tone & detoxify your whole body, we've got a treatment for you.",
+        description: "Body treatments care for the skin of the whole body!",
+        description1: "Whether you're looking to slough your skin to perfection, or tone & detoxify your whole body, we've got a treatment for you.",
         button: "Learn More",
     },
     {
-        title: "Body treatments care for the skin of the whole body!",
-        subtitle: "Whether you're looking to slough your skin to perfection, or tone & detoxify your whole body, we've got a treatment for you.",
+        description1: "Body treatments care for the skin of the whole body!",
+        description2: "Whether you're looking to slough your skin to perfection, or tone & detoxify your whole body, we've got a treatment for you.",
         description: "Lorem Ipsum is simply dummy text of the printing...."
     }
 ];
 
 function Spa() {
     return (
-        <div className="card-container container-fluid">
+        <div className="card-container ">
             <div className="row">
                 {cardData.map((card, index) => (
-                    <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={index}>
+                    <div className="col-lg-4 col-md-4 col-sm-6 mb-4" key={index}>
                         <div className="card custom-card">
                             <div className="card-body">
-                                <h5 className="card-title1"><b>{card.title}</b></h5>
-                                <h5 className="card-title1"><b>{card.subtitle}</b></h5>
-                                {card.description && <h5 className="card-title1"><b>{card.description}</b></h5>}
-                                {card.button && <button className="btn btn-primary float-start">{card.button}</button>}
+                            <h5 className="card-title">
+                    <b>{card.title}</b>
+                    <br />
+                    <span className="card-title"><b>{card.title2}</b></span>
+                    <br />
+                    <span className="card-title"><b>{card.title3}</b></span>
+                </h5>
+                                <h6 className="card-title"><i>{card.subtitle}</i></h6>
+                                {card.description1 && (
+                                 <p className={`card-title ${index === 1 ? 'bold-text' : ''}`}>{card.description1}</p>
+                                )}
+                                {card.description2 && (
+                                <p className={`card-title ${index === 1 ? 'bold-text' : ''}`}>{card.description2}</p>
+                                )}
+                                {card.description && (
+                                <p className={`card-title ${index === 1 ? 'bold-text' : ''}`}>{card.description}</p>
+                                )}
+                                {card.button && <button className="btn btn-dark">{card.button}</button>}
                             </div>
                         </div>
                     </div>
@@ -38,7 +53,5 @@ function Spa() {
         </div>
     );
 }
-
-
 
 export default Spa;
